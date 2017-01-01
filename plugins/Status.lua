b4986ab7a2c8f32497dc227cb49a8cd4734c446a
Electrovirus <E.electrovirus@gmail.com>
2017-01-01 15:58:08 +0330

    TB | plugins

--- plugins/Status.lua
+++ plugins/Status.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
-local text = io.popen("sh ./data/cmd.sh"):read('*all')
+local text = io.popen("sh ./core/status.sh"):read('*all')
 if is_sudo(msg) then
   return text
 end
   end
 return {
   patterns = {
-    '^[!/]serverinfo$'
+    '^[!/]status$'
   },
   run = run,
   moderated = true
