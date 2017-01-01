0170000cfbdc7be8916097acda960ef9b0378438
Electrovirus <E.electrovirus@gmail.com>
2017-01-01 14:48:02 +0330

    TB | plugins

--- plugins/sh.lua
+++ plugins/sh.lua
@@ -4,7 +4,7 @@ local text = io.popen(matches[1]):read('*all')
   return text
 end
  if not is_sudo(msg) then
-local text = "⏹Needs sudo acces\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n📶Local sudo∶Electrovirus"
+local text = "🔴Needs sudo acces\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n🔱Local sudo∶Electrovirus"
   return text
 end
   end
