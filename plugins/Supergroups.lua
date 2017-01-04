60c99d86ad094d731e71d5660512bdd5e0f36a51
Electrovirus <E.electrovirus@gmail.com>
2017-01-05 01:52:54 +0330

    TB | plugins

--- plugins/Supergroups.lua
+++ plugins/Supergroups.lua
@@ -2694,7 +2694,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "🔗You can see bot commands in\n @Commands_ch"
+			text = ""
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
