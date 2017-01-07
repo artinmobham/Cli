8107de45c5d168422f3e283c78b2558f0909bdbb
Electrovirus <E.electrovirus@gmail.com>
2017-01-07 16:10:16 +0330

    Create Info.lua

---
+++ plugins/Info.lua
@@ -0,0 +1,19 @@
+do
+function run(msg, matches)
+  if matches[1] == 'userid' then
+return "#⃣User info\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\nℹ️Status∶Recognized✔\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n📍FirstName | "..msg.from.first_name.."\n📍LastName | "..msg.from.last_name.."\n📍Username | @"..msg.from.username.."\n📍Identification | "..msg.from.id.."\n📍UserLink | t.me/"..msg.from.username..""
+end
+  if matches[1] == 'gpid' then
+return "#⃣Group info\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\nℹ️Status∶Moderated✔\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n📍GroupName | "..msg.to.title.."\n📍Identification | "..msg.to.id.."\n📍UserName | Not available in your region"  
+end
+  if matches[1] == 'msgid' then
+return "#⃣Message info\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\nℹ️Status∶Sent✔\n┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n📍Message text | "..msg.text.."\n📍Identification | "..msg.id..""
+end
+end
+return {
+  patterns = {
+    "^[!#/](.*)$",
+  },
+  run = run
+}
+end
