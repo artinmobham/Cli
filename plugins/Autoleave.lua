00440f00e35c031d05b6b748151c104ea104a989
Electrovirus <E.electrovirus@gmail.com>
2017-01-07 21:08:42 +0330

    TB | plugins

--- plugins/Autoleave.lua
+++ plugins/Autoleave.lua
@@ -3,7 +3,7 @@ do
 local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
-    if matches[1] == 'ban group' and is_admin1(msg) then
+    if matches[1] == 'gpblock' and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
@@ -15,7 +15,7 @@ end
  
 return {
   patterns = {
-    "^[/#!](ban) (group)$",
+    "^[/#!](gpblock)$",
     "^!!tgservice (.+)$",
   },
   run = run
